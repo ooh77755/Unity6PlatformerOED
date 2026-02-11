80e083aa21a3d8e4567f6415bcb8312c59b672d0
DESKTOP-TD6F7D7\Alan <alan.stewart@port.ac.uk>
2026-02-11 13:27:39 +0000

    Player moves on Y

--- Assets/Z_Scripts/PlayerController.cs
+++ Assets/Z_Scripts/PlayerController.cs
@@ -7,10 +7,14 @@ public class PlayerController : MonoBehaviour
     Vector2 moveInput;
 
     [SerializeField] Rigidbody2D rb;
+    [SerializeField] BoxCollider2D myFeetCollider;
+
+    [SerializeField] bool isJumpReq;
 
     private void FixedUpdate()
     {
         Running();
+        Jumping();
     }
 
     void OnMove(InputValue value)
@@ -18,9 +22,24 @@ public class PlayerController : MonoBehaviour
         moveInput = value.Get<Vector2>();
     }
 
+    void OnJump(InputValue value)
+    {
+        if(value.isPressed)
+        {
+            isJumpReq = true;
+        }
+    }
+
     private void Running()
     {
         Vector2 playerVelocity = new Vector2(moveInput.x * 5f, rb.linearVelocity.y);
         rb.linearVelocity = playerVelocity;
+    }
+
+    private void Jumping()
+    {
+        if (!isJumpReq) return;
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, 28f);
+        isJumpReq = false;
     }
 }
